d8e22bd790139e6079f20a1941d93ee172816355
Juan Carlos Valerio Barreto <jc.valerio.b@gmail.com>
2016-11-07 17:19:33 -0600

    cambio de texto en componente

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'my-app',
-	template: '<h1>My First Angular App</h1>'
+	template: '<h1>My First Angular App with SystemJS</h1>'
 })
 
-export class AppComponent {
-	
-}
+export class AppComponent { }
